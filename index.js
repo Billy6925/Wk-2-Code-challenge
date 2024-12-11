76883dddcf57670bb4a46abb8594e8dcd0248dee
Billy Eskein <billyeskein@gmail.com>
2024-12-11 05:26:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
      //Function to display items in the list
      const displayItems = () => {
-        { shoppingList.innerHTML = '';//clear the current list
+        shoppingList.innerHTML = '';//clear the current list
             items.forEach((item, index) => {
                 const listItem = document.createElement('li'); 
                 listItem.textContent = item.text;
                 if (item.purchased) { 
-                    listItem.style.textDecoration = "line-through";
-                     listItem.style.backgroundColor = "#d3d3d3"; // Light grey background 
-                     }
+                listItem.style.textDecoration = "line-through";
+                 listItem.style.backgroundColor = "#d3d3d3"; // Light grey background 
+                  }
+                 listItem.addEventListener('click', () => markAsPurchased(index));
+                shoppingList.appendChild(listItem); 
+                    });
+                };
+                //Function to add a new item to the list
+                const addItem = () => { 
+                const itemText = itemInput.value.trim();
+                if (itemText !== "") {
+
