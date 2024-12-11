26848e8df6219a44990f38237bc48129fb672593
Billy Eskein <billyeskein@gmail.com>
2024-12-11 05:11:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const itemInput = document.getElementById('itemInput');
+    const addButton = document.getElementById('itemButton');
+    const shoppingList = document.getElementById('itemList');
+    
