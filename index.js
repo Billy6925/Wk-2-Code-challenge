d5187b455aa1a3df674731fddbffd38c2b688fe4
Billy Eskein <billyeskein@gmail.com>
2024-12-11 05:18:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const shoppingList = document.getElementById('itemList');
     const clearButton = document.getElementById('clearButton');
      let items = []; 
+
+     //Function to display items in the list
+     const displayItems = () => {
+        { shoppingList.innerHTML = '';//clear the current list
+            items.forEach((item, index) => {
+                const listItem = document.createElement('li'); 
+                listItem.textContent = item.text;
